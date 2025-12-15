481860b4ebd6d2499f0a9f3eeab0d52c504dfe3a
Chuseok22 <bjh59629@naver.com>
2025-12-15 17:08:46 +0900

    project init

---
+++ src/main/java/xyz/romrom/githubissueingestor/GithubIssueIngestorApplication.java
@@ -0,0 +1,13 @@
+package xyz.romrom.githubissueingestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubIssueIngestorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GithubIssueIngestorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/xyz/romrom/githubissueingestor/GithubIssueIngestorApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.romrom.githubissueingestor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubIssueIngestorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
